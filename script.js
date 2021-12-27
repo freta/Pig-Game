c7cff823e7d5725b6a0c705a4bb5515fd28e822b
freta <[itsmefruta@gmail.com]>
2021-12-27 23:54:54 +0300

    Adding the new game function to reset every score

--- script.js
+++ script.js
@@ -8,15 +8,7 @@ hold - The turn total is added to the player's score and it becomes the opponent
 ****************************************************/
 
 var dice, scores, roundScore, activePlayer;
-scores = [0, 0];
-roundScore = 0;
-activePlayer = 0;
-
-document.querySelector('.dice').style.display = 'none';
-document.getElementById('score-0').textContent = '0';
-document.getElementById('score-1').textContent = '0';
-document.getElementById('current-0').textContent = '0';
-document.getElementById('current-1').textContent = '0';
+init();
 
 document.querySelector('.btn-roll').addEventListener('click', function () {
   //random number
@@ -58,6 +50,25 @@ document.querySelector('.btn-hold').addEventListener('click', function () {
     nextPlayer();
   }
 });
+document.querySelector('.btn-new').addEventListener('click', init);
+function init() {
+  scores = [0, 0];
+  activePlayer = 0;
+  roundScore = 0;
+
+  document.querySelector('.dice').style.display = 'none';
+  document.getElementById('score-0').textContent = '0';
+  document.getElementById('score-1').textContent = '0';
+  document.getElementById('current-0').textContent = '0';
+  document.getElementById('current-1').textContent = '0';
+  document.getElementById('name-0').textContent = 'Player 1';
+  document.getElementById('name-1').textContent = 'Player 2';
+  document.querySelector('.player-0-panel').classList.remove('winner');
+  document.querySelector('.player-1-panel').classList.remove('winner');
+  document.querySelector('.player-0-panel').classList.remove('active');
+  document.querySelector('.player-1-panel').classList.remove('active');
+  document.querySelector('.player-0-panel').classList.add('active');
+}
 
 function nextPlayer() {
   //next player
