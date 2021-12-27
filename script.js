475b21ea918c0cda75c69220d1e37a6cd47bd55d
freta <[itsmefruta@gmail.com]>
2021-12-27 22:36:26 +0300

    Developing the interactivity of the game with in javescript

--- script.js
+++ script.js
@@ -4,5 +4,40 @@
 roll - If the player rolls a
 1: the player scores nothing and it becomes the opponent's turn.
 2 - 6: the number is added to the player's turn total and the player's turn continues.
-hold - The turn total is added to the player's score and it becomes the opponent's turn. */
+hold - The turn total is added to the player's score and it becomes the opponent's turn. 
+****************************************************/
 
+var dice, scores, roundScore, activePlayer;
+scores = [0, 0];
+roundScore = 0;
+activePlayer = 0;
+
+document.querySelector('.dice').style.display = 'none';
+document.getElementById('score-0').textContent = '0';
+document.getElementById('score-1').textContent = '0';
+document.getElementById('current-0').textContent = '0';
+document.getElementById('current-1').textContent = '0';
+
+document.querySelector('.btn-roll').addEventListener('click', function () {
+  //random number
+  dice = Math.floor(Math.random() * 6) + 1;
+  //display the result
+  var diceDOM = document.querySelector('.dice');
+  diceDOM.style.display = 'block';
+  diceDOM.src = 'resources/dice-' + dice + '.png';
+  //update the round score if the rolled number was NOT a 1
+  if (dice !== 1) {
+    // add score
+    roundScore += dice;
+    document.querySelector('#current-' + activePlayer).textContent = roundScore;
+  } else {
+    // next player
+    activePlayer === 0 ? (activePlayer = 1) : (activePlayer = 0);
+    roundScore = 0;
+    document.getElementById('current-0').textContent = '0';
+    document.getElementById('current-1').textContent = '0';
+    document.querySelector('.player-0-panel').classList.toggle('active');
+    document.querySelector('.player-1-panel').classList.toggle('active');
+    document.querySelector('.dice').style.display = 'none';
+  }
+});
